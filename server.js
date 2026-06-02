706152a1bf478c7d0372f55f689fcd9a53cc4aba
Dharsan <thehonoredone672@gmail.com>
2026-06-02 17:04:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+// server.js
 require("dotenv").config();
-
 const express = require("express");
 const path = require("path");
 
@@ -10,16 +10,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /* Static Files */
+// This serves static assets like CSS, images, and JS files from the public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-/* Routes */
+/* API Routes */
 const contactRoute = require("./routes/contact");
 app.use("/api/contact", contactRoute);
 
-/* Homepage */
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+/* --- HTML Page Routes --- */
+
+// Homepage
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
+app.get("/home", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
+
+// Main Pages
+app.get("/products-services", (req, res) => res.sendFile(path.join(__dirname, "public", "products-services.html")));
+app.get("/contact", (req, res) => res.sendFile(path.join(__dirname, "public", "contact.html")));
+app.get("/about", (req, res) => res.sendFile(path.join(__dirname, "public", "about.html")));
+
+// Team / Profile Pages
+app.get("/dharsan", (req, res) => res.sendFile(path.join(__dirname, "public", "dharsan.html")));
+app.get("/naveenkumar", (req, res) => res.sendFile(path.join(__dirname, "public", "naveenkumar.html")));
+app.get("/arun", (req, res) => res.sendFile(path.join(__dirname, "public", "arun.html")));
+// app.get("/anton", (req, res) => res.sendFile(path.join(__dirname, "public", "anton.html")));
+
+/* ------------------------ */
 
 /* Server */
 const PORT = process.env.PORT || 5000;
